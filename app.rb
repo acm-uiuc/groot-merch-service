3f7d37263a2a5f0b4e843d8eead4b8694802e8db
Sameet Sapra <sameetandpotatoes@gmail.com>
2017-04-24 17:59:52 -0500

    Change yaml files

--- app.rb
+++ app.rb
@@ -41,7 +41,7 @@ class GrootMerchService < Sinatra::Base
     configure :development do
         enable :unsecure
 
-        db = Config.load_config("development")
+        db = Config.load_config("development_db")
         DataMapper::Logger.new($stdout, :debug)
         DataMapper.setup(
             :default,
@@ -56,7 +56,7 @@ class GrootMerchService < Sinatra::Base
     end
 
     configure :test do
-        db = Config.load_config("test")
+        db = Config.load_config("test_db")
         DataMapper.setup(
             :default,
             "mysql://" + db["user"] + ":" + db["password"] + "@" + db["hostname"]+ "/" + db["name"]
@@ -67,7 +67,7 @@ class GrootMerchService < Sinatra::Base
     configure :production do
         disable :unsecure
         
-        db = Config.load_config("production")
+        db = Config.load_config("production_db")
         DataMapper.setup(
             :default,
             "mysql://" + db["user"] + ":" + db["password"] + "@" + db["hostname"]+ "/" + db["name"]
